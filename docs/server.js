2c63f5b8c0ccc4724924b4046073b3fe4b86a7a8
velvet-jedi <pranjalruhela2811@gmail.com>
2024-03-11 19:05:51 +0530

    db connected

--- docs/server.js
+++ docs/server.js
@@ -14,8 +14,7 @@ app.use(express.json());
 const connectDB = (url) => {
     return mongoose.connect(url)
 }
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 
 const start = async () => {
